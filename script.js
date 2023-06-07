0d90a844263901a1905fb5744617e566a06f9bd0
Harunsener <132216125+Harunsener@users.noreply.github.com>
2023-06-07 16:53:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function playMusic() {
+    var audio = document.getElementsByTagName("audio")[5];
+    audio.play();
+  }
+  
+  function pauseMusic() {
+    var audio = document.getElementsByTagName("audio")[0];
+    audio.pause();
+  }
+  
